8e06cc45a6bdab947ad38ccb81a8bd85f4851054
Lucas Sousa Meireles <meireles31@msn.com>
2016-01-30 14:55:19 +0000

    Corrected distance error

--- Speaker.py
+++ Speaker.py
@@ -6,7 +6,9 @@ class Object3D():
         self.y = y
         self.z = z
     def distanceTo(self, anotherPoint):
-        return math.sqrt( (self.x - anotherPoint.x)**2 + (self.y - anotherPoint.y)**2 + (self.y - anotherPoint.y)**2)
+        return math.sqrt( (self.x - anotherPoint.x)**2 + (self.y - anotherPoint.y)**2 + (self.z - anotherPoint.z)**2)
+    def __repr__(self):
+        return "X:%s Y:%s Z:%s" % (self.x, self.y, self.z)
 
 class Speaker(Object3D):
     pass
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def finalPoint(pointList):
 	finalY = 0.0
 	finalZ = 0.0
 	weight = 0.0
-	for pair_in in pointList:
+	for pair_in in pointList[0:len(pointList)/10]:
 		finalX += pair_in[1].x * (1/pair_in[0])
 		finalY += pair_in[1].y * (1/pair_in[0])
 		finalZ += pair_in[1].z * (1/pair_in[0])
@@ -36,7 +36,7 @@ def multilateration_thread( SpeakersList,  iStart, iStop, jStart, jStop, kStart,
                 testAD  = distToSpeakerA - distToSpeakerD
                 testBD  = distToSpeakerB - distToSpeakerD
                 testCD  = distToSpeakerC - distToSpeakerD
-                accuracy = math.sqrt( (distAB - testAB)**2 + (distBC - testBC)**2 + (distAC - testAC)**2 + (distAD - testAD)**2 + (distBD -  testBD)**2 + (distCD - testCD)**2 )
+                accuracy = math.sqrt( ((distAB - testAB)**2) + ((distBC - testBC)**2) + ((distAC - testAC)**2) + ((distAD - testAD)**2) + ((distBD -  testBD)**2) + ((distCD - testCD)**2) )
                 if accuracy <= 1.0:
                     entry = (accuracy, currentPoint)
                     fPointList.append(entry)
@@ -50,39 +50,37 @@ def main():
     SpeakerC = Speaker(0, 20, 0)
     SpeakerD = Speaker(5, 10, 5)
     SpeakersList = [SpeakerA, SpeakerB, SpeakerC, SpeakerD]
-    target = Object3D(3.3, 14.6, 2.9)
+    target = Object3D(9.2, 7.2, 3.1)
 
     #Calculating needed constants
-    timeA = target.distanceTo(SpeakerA)/ speedOfSound
-    timeB = target.distanceTo(SpeakerB)/ speedOfSound
-    timeC = target.distanceTo(SpeakerC)/ speedOfSound
-    timeD = target.distanceTo(SpeakerD)/ speedOfSound
+    timeA = target.distanceTo(SpeakerA) / speedOfSound
+    timeB = target.distanceTo(SpeakerB) / speedOfSound
+    timeC = target.distanceTo(SpeakerC) / speedOfSound
+    timeD = target.distanceTo(SpeakerD) / speedOfSound
     timeAB = timeA - timeB
     timeBC = timeB - timeC
     timeAC = timeA - timeC
     timeAD = timeA - timeD
     timeBD = timeB - timeD
     timeCD = timeC - timeD
-    distAB  = timeAB*speedOfSound
-    distBC  = timeBC*speedOfSound
-    distAC  = timeAC*speedOfSound
-    distAD  = timeAD*speedOfSound
-    distBD  = timeBD*speedOfSound
-    distCD  = timeCD*speedOfSound
+    distAB  = timeAB * speedOfSound
+    distBC  = timeBC * speedOfSound
+    distAC  = timeAC * speedOfSound
+    distAD  = timeAD * speedOfSound
+    distBD  = timeBD * speedOfSound
+    distCD  = timeCD * speedOfSound
 
     #assign 3 threads to do the multilateration calculation on different ranges
-    thread1 = pool.apply( multilateration_thread, (SpeakersList, 0.0, 10.0, 0.0, 7.0, 0.0, 5.0, distAB, distBC, distAC, distAD, distBD, distCD,) )
-    thread2 = pool.apply( multilateration_thread, (SpeakersList, 0.0, 10.0, 7.0, 14.0, 0.0, 5.0, distAB, distBC, distAC, distAD, distBD, distCD,) )
-    thread3 = pool.apply( multilateration_thread, (SpeakersList, 0.0, 10.0, 14.0, 20.0, 0.0, 5.0, distAB, distBC, distAC, distAD, distBD, distCD,) )
+    thread1 = pool.apply_async( multilateration_thread, (SpeakersList, 0.0, 10.0, 0.0, 7.0, 0.0, 5.0, distAB, distBC, distAC, distAD, distBD, distCD,) )
+    thread2 = pool.apply_async( multilateration_thread, (SpeakersList, 0.0, 10.0, 7.0, 14.0, 0.0, 5.0, distAB, distBC, distAC, distAD, distBD, distCD,) )
+    thread3 = pool.apply_async( multilateration_thread, (SpeakersList, 0.0, 10.0, 14.0, 20.0, 0.0, 5.0, distAB, distBC, distAC, distAD, distBD, distCD,) )
     thread1Return = thread1.get(timeout=3)
     thread2Return = thread2.get(timeout=3)
     thread3Return = thread3.get(timeout=3)
     pointList = thread1Return + thread2Return + thread3Return
     pointList.sort()
-    finalPointOut = finalPoint()
-    print finalPointOut.x
-    print finalPointOut.y
-    print finalPointOut.z
+    finalPointOut = finalPoint(pointList)
+    print finalPointOut
 
     #c.send(finalPoint(pointList))
 
